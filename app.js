f739ec83bd78da3e6f661b17cd2392ef1be78d3a
brilu-22 <231214@virtualwindow.co.za>
2024-08-20 15:07:01 +0200

    commit

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const sign_in_btn = document.querySelector("#sign-in-btn");
-const sign_up_btn = document.querySelector("#sign-up-btn");
+const signUpButton = document.getElementById("sign-up-btn");
+const signInButton = document.getElementById("Sign-in-btn");
 const container = document.querySelector(".container");
 
-sign_up_btn.addEventListener("click", () => {
+// Event listeners for buttons
+signUpButton.addEventListener("click", () => {
   container.classList.add("sign-up-mode");
 });
 
-sign_in_btn.addEventListener("click", () => {
+signInButton.addEventListener("click", () => {
   container.classList.remove("sign-up-mode");
 });
